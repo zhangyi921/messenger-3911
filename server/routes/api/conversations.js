7e97a47aa9fc8f6f49757c27d8dd9c773822eda8
Yi Zhang <yi16@ualberta.ca>
2021-08-27 00:27:39 -0600

    Update conversations.js

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const { User, Conversation, Message } = require("../../db/models");
 const { Op } = require("sequelize");
 const onlineUsers = require("../../onlineUsers");
-const Sequelize = require("sequelize");
 
 // get all conversations for a user, include latest message text for preview, and all messages
 // include other user model so we have info on username/profile pic (don't include current user info)
